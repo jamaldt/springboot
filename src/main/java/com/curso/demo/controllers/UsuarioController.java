86a5bae899e9838617ea9fbc8029b0d8c67017fc
jamal <carldeltoro@gmail.com>
2022-01-09 07:25:12 -0400

    Servicio REST usuario

--- src/main/java/com/curso/demo/controllers/UsuarioController.java
+++ src/main/java/com/curso/demo/controllers/UsuarioController.java
@@ -1,5 +1,6 @@
 package com.curso.demo.controllers;
 
+import com.curso.demo.models.Usuario;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +21,16 @@ public class UsuarioController
     @RequestMapping(value = "frutas")
     public List<String> frutas(){
         return List.of("manzana","pera","durazno");
+    }
+
+     @RequestMapping(value = "usuario")
+    public Usuario getUsuario(){
+        Usuario usuario = new Usuario();
+        usuario.setApellido("Del Toro");
+        usuario.setEmail("Carldeltoro@gmail.com");
+        usuario.setPassword("134$%·");
+        usuario.setTelefono("+57-23434554");
+        usuario.setTelefono("carlos");
+        return usuario;
     }
 }
---
+++ src/main/java/com/curso/demo/models/Usuario.java
@@ -0,0 +1,74 @@
+package com.curso.demo.models;
+
+/**
+ * System:                 CleanBnB
+ * Name:                   Usuario
+ * Description:            Class that represents a Usuario's Entity in the application
+ *
+ * @author carlosdeltoro
+ * @version 1.0
+ * @since 1/9/22
+ */
+public class Usuario
+{
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String telefono;
+    private String password;
+
+    public String getNombre()
+    {
+        return nombre;
+    }
+
+    public Usuario setNombre(String nombre)
+    {
+        this.nombre = nombre;
+        return this;
+    }
+
+    public String getApellido()
+    {
+        return apellido;
+    }
+
+    public Usuario setApellido(String apellido)
+    {
+        this.apellido = apellido;
+        return this;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public Usuario setEmail(String email)
+    {
+        this.email = email;
+        return this;
+    }
+
+    public String getTelefono()
+    {
+        return telefono;
+    }
+
+    public Usuario setTelefono(String telefono)
+    {
+        this.telefono = telefono;
+        return this;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public Usuario setPassword(String password)
+    {
+        this.password = password;
+        return this;
+    }
+}
